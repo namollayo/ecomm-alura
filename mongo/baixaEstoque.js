97dc4e7da61c79f35a7a4b916f4f16d0b9f1a804
Gabi Carrara <118398244+namollayo@users.noreply.github.com>
2023-06-26 11:18:33 -0300

    Delete mongo directory

--- mongo/baixaEstoque.js
+++
@@ -1,9 +0,0 @@
-use('ecomm')
-
-const result = db.products.updateOne({
-    nome: 'Galaxy Tab S8', 
-    estoque: {$gte: 2}}, 
-    {$inc: {estoque: -2}
-})
-
-console.log(result)
--- mongo/listaCategoriasAtivas.js
+++
@@ -1,5 +0,0 @@
-use("ecomm");
-
-var categories = db.categories.find({status: "ATIVA"});
-
-console.log(categories);
